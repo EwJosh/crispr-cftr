0b31e8466a8b966ab2bccb0ff99ff44fdca69679
Pistauer, Moritz Robert Karl <moritz.pistauer@student.tugraz.at>
2022-12-14 22:16:40 -0800

    working scoring

--- grna_grader.py
+++ grna_grader.py
@@ -25,47 +25,27 @@ MM/DD/22:
     AUTHOR:  Moritz Pistauer
     COMMENT: No comments
 
+12/14/22:
+    MOD:     Calculate scores
+    AUTHOR:  Moritz Pistauer
+    COMMENT: No comments
+
 ====================== END OF MODIFICATION HISTORY ============================
 """
 
 from Bio import Entrez
 from Bio import SeqIO
-import xmltodict
+# import pandas as pd
 
-
-def check_for_repetitive_bases(grna):
-    base_counts = {"A": 0, "C": 0, "G": 0, "U": 0}
-
-    for base in grna:
-        base_counts[base] += 1
-
-    for base, count in base_counts.items():
-        if count >= 5:
-            return True
-
-    return False
-
-
-# Function to calculate the GC content of a gRNA
-def calculate_gc_content(grna):
-    # Count the number of G and C nucleotides in the gRNA
-    gc_count = grna.count('G') + grna.count('C')
-
-    # Calculate the GC content as a percentage
-    gc_content = gc_count / len(grna)
-
-    return gc_content
 
 # Function to check for off-target sites in a gRNA sequence
 def off_target_sites_found(grna, genome):
     # Loop through every substring of the same length as the gRNA in the reference genome
     for i in range(len(genome) - len(grna) + 1):
         substring = genome[i:i + len(grna)]
-        # If the gRNA sequence matches the substring, return True
         if grna == substring:
             return True
 
-    # If no matching substring is found, return False
     return False
 
 
@@ -82,20 +62,10 @@ def quality_of_grna(grna):
         print('Invalid: pam sequnece contains C or T')
         return False
 
-    # Check for off-target sites (not implemented in this example)
-    if off_target_sites_found(grna):
-        print('Invalid: gRNA has off-target sites.')
-        return False
-
     # Check for GC content
     gc_content = calculate_gc_content(grna)
     if gc_content < 0.3 or gc_content > 0.7:
         print('Invalid: gRNA has GC content outside the acceptable range 30% - 70%.')
-        return False
-
-    # Check for repetitive bases like AAAAA, CCCCC, GGGGG, UUUUU
-    if check_for_repetitive_bases(grna):
-        print('gRNA continues repetitive bases of length 5')
         return False
 
     return True
@@ -106,8 +76,6 @@ def read_grna_file():
     seq_reader = open('grna_sequences.txt')
     seq_lines = []
 
-    # NOTE: idk how y'all want the logic on how to format/read the txt file, but this works for the last programming assignment
-    
     for line in seq_reader:
         if line[0:4] == '>Seq':
             continue
@@ -117,6 +85,95 @@ def read_grna_file():
     seq_reader.close()
 
     return seq_lines
+
+
+def count_repetitive_bases(grna):
+    base_counts = {"A": 0, "C": 0, "G": 0, "T": 0}
+    prev_base = ''
+    count = 1
+
+    for base in grna:
+        if base == prev_base:
+            count += 1
+            if count > base_counts[base]:
+                base_counts[base] = count
+        else:
+            count = 1
+
+        prev_base = base
+
+    return base_counts
+
+
+def score_repetitive_bases(grna_seqs):
+    repetition_scores = {}
+    for grna in grna_seqs:
+        max_rep_count = count_repetitive_bases(grna)
+        max_rep = max(max_rep_count.values())
+        repetition_scores.setdefault(grna, round(10/max_rep, 2))
+    return repetition_scores
+
+
+# Function to score the GC content of a gRNA
+# The lower the GC count is compared to 0.57, the bigger the absolute distance the higher the score
+def calculate_gc_content(grna_seqs):
+    gc_content_scores = {}
+    for grna in grna_seqs:
+        gc_count = grna.count('G') + grna.count('C')
+        gc_content_percent = gc_count / len(grna)
+        gc_content_scores.setdefault(grna, gc_content_percent)
+
+    # Normalize the scores (max_score = 10)
+    for gc_score in gc_content_scores:
+        distance = abs(gc_content_scores[gc_score]-0.57)/0.57
+        gc_content_scores[gc_score] = round(10 * (1 - distance), 2)
+
+    return gc_content_scores
+
+
+# Position-specific sequence
+# Count the number of 'C' and 'T' on the last 10 positions and score it
+def score_position(grna_seqs):
+    position_scores = {}
+
+    for grna in grna_seqs:
+        last_10 = grna[-10:]
+        c_count = last_10.count('C')
+        t_count = last_10.count('T')
+        position_scores.setdefault(grna, c_count + t_count)
+
+    min_count = min(position_scores.values())
+
+    # Normalize the scores (max_score = 10)
+    for element in position_scores:
+        if position_scores[element] != 0:
+            position_scores[element] = round(10*(min_count/position_scores[element]), 2)
+
+    return position_scores
+
+
+OUTPUT_FILE = 'grna_scores.txt'
+# Calculate the total scores
+# The results will be written into grna_scores.txt file
+def score(grna_seqs):
+    overall_scores = {}
+    position_scores = score_position(grna_seqs)
+    gc_content_scores = calculate_gc_content(grna_seqs)
+    repetitive_base_scores = score_repetitive_bases(grna_seqs)
+
+    print('Outputing results file...')
+    with open(OUTPUT_FILE, 'w') as f:
+        for grna in grna_seqs:
+            overall_scores[grna] = round(position_scores[grna] + gc_content_scores[grna] + repetitive_base_scores[grna], 2)
+            output = f'\n-------------------------------------' \
+                     f'\nsRNA Sequence: {grna}\n\n' \
+                     f'Scoring Metrics:\n\tPosition Score: {position_scores[grna]}/10.0\n\t' \
+                     f'GC Content Score: {gc_content_scores[grna]}/10.0\n\t' \
+                     f'Repetitive Base Score : {repetitive_base_scores[grna]}/10.0\n' \
+                     f'==> Assessment Score: {overall_scores[grna]}/30.0\n\n'
+            f.write(output)
+    print(f'Results output at: {OUTPUT_FILE}')
+    return overall_scores
     
 
 #MAIN Function
@@ -160,19 +217,21 @@ def main():
 
     file_type = 'fasta' # either 'fasta' or 'gb' (GenBank)    ... not sure which we want yet
 
-    handle = Entrez.efetch(db = 'nuccore', id='NC_000007.14', rettype=file_type)
-    chromosome = SeqIO.read(handle, format=file_type)
-    handle.close()
+    # handle = Entrez.efetch(db = 'nuccore', id='NC_000007.14', rettype=file_type)
+    # chromosome = SeqIO.read(handle, format=file_type)
+    # handle.close()
     # chromosome is of the Seq class from Biopython
     # https://biopython.org/wiki/Seq
 
     ## Don't print chomosome.seq. It's insanely way too long.
     # print('chromosome.id', chromosome.id)
     # print('chromosome.seq:', chromosome.seq)
-    print('chromosome seven:', chromosome)
+    # print('chromosome seven:', chromosome)
 
 
     # 3) Score each gRNA
+
+    score(grna_seqs)
     
 
     print('+===  END  ===+')
